5372fcb0007d77643d86ecc794241ed0eaf9845e
Ichrafsassi <ichraf.sassi.456@gmail.com>
2024-02-23 02:59:29 +0100

    auth+main

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,13 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+This module provides a simple authentication mechanism for the application.
+"""
+
 import bcrypt
-import uuid
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
     """Returns a salted hash of the input password."""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """Returns a string representation of a new UUID."""
+    return str(uuid.uuid4())
 
 
 class Auth:
@@ -19,10 +30,13 @@ class Auth:
 
     def register_user(self, email: str, password: str) -> User:
         """Register a user in the database."""
-        if self._db.find_user_by(email=email):
-            raise ValueError(f"User {email} already exists")
-        hashed_password = self._hash_password(password)
-        return self._db.add_user(email, hashed_password)
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f"User {email} already exists")
+
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
 
     def valid_login(self, email: str, password: str) -> bool:
         """Validate a user's credentials."""
@@ -35,30 +49,46 @@ class Auth:
 
     def create_session(self, email: str) -> str:
         """Create a new session for the user"""
-        session_id = str(uuid.uuid4())
-        self._db.update_user_by_email(email, session_id=session_id)
-        return session_id
+        try:
+            session_id = _generate_uuid()
+            user = self._db.find_user_by(email=email)
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
 
-    def get_user_from_session_id(self, session_id: str) -> User:
+    def get_user_from_session_id(self, session_id: str) -> str:
         """Get a user from a session_id"""
-        return self._db.find_user_by(session_id=session_id)
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
 
     def destroy_session(self, user_id: int) -> None:
         """Destroy a session"""
-        self._db.update_user(user_id, session_id=None)
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            return None
 
     def get_reset_password_token(self, email: str) -> str:
         """Get a reset password token"""
-        token = str(uuid.uuid4())
-        self._db.update_user_by_email(email, reset_token=token)
-        return token
+        try:
+            user = self._db.find_user_by(email=email)
+            token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=token)
+            return token
+        except NoResultFound:
+            raise ValueError
 
     def update_password(self, reset_token: str, password: str) -> None:
         """Update a user's password"""
-        hashed_password = self._hash_password(password)
-        self._db.update_user_by_reset_token(reset_token,
-                                            hashed_password, reset_token=None)
-
-    def _hash_password(self, password: str) -> bytes:
-        """Returns a salted hash of the input password."""
-        return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            self._db.update_user(user.id,
+                                 hashed_password=_hash_password(password))
+            self._db.update_user(user.id, reset_token=None)
+        except NoResultFound:
+            raise ValueError
+        return None
